40c6d73d2f1e6ba0c864edf38d5581112816d760
Victor Trejo <vic3jo@gmail.com>
2024-05-08 08:33:08 -0400

    Fix spacing problem

--- app/service_turn/router.py
+++ app/service_turn/router.py
@@ -11,13 +11,14 @@ from .constants import (
     GET_TURNS_STATUS_TABLE_OPERATION_ID,
     GET_TURN_AUDIO_OPERATION_ID,
     TURNS_STATUS_TABLE_PATH,
-    TURN_AUDIO_PATH
+    TURN_AUDIO_PATH,
 )
 from . import handlers
 from . import models
 
 
 router = APIRouter()
+
 
 @router.get(
     TURNS_STATUS_TABLE_PATH,
@@ -43,7 +44,7 @@ def get_turns_status_table(
     response_class=FileResponse,
     responses=responses.responses_descriptions,
 )
-def get_turn_audio(turn_name: str) -> FileResponse: 
+def get_turn_audio(turn_name: str) -> FileResponse:
     """Gets the audio of a turn"""
     path = handlers.get_turn_audio(turn_name)
     return FileResponse(path, filename="audio.mp3", media_type="audio/mpeg")
